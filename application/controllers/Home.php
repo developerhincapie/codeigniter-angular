64db7df036446a9ad542901e1ef5d14b946dd885
Yomar Miranda <mirandayomar@gmail.com>
2016-12-04 17:46:51 -0500

    Se creo el constructor del controlador Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,8 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+	public function __construct(){
+		parent::__construct();
+	}
 	
 	public function index(){
-		$this->load->view('');
+		echo 'Hola Mundo!';
 	}
 }
